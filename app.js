cb1afc85e9ff14153c083359c15dd490b586f29d
ruby0143 <mysoresriharsha07@gmail.com>
2022-03-29 11:34:51 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,115 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use("/public",express.static("public"));
+
+mongoose.connect("mongodb+srv://ruby07:8074662205s@cluster0.97u8x.mongodb.net/eventDB", {useNewUrlParser: true});
+
+const formSchema = {
+    eventName : String,
+    t1Name : String,
+    t1Rno : String,
+    t2Name : String,
+    t2Rno : String,
+    t3Name : String,
+    t3Rno : String,
+    t4Name : String,
+    t4Rno : String,
+    email : String,
+    mobile : Number
+};
+
+const rUser = mongoose.model("RUser",formSchema);
+
+
+
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/tech",(req,res)=>{
+    res.render("tech");
+});
+
+app.get("/nontech",(req,res)=>{
+    res.render("nontech");
+});
+
+app.get("/mba",(req,res)=>{
+    res.render("mba");
+});
+
+app.get("/register/:event/:key",function(req,res){
+    const event = req.params.event;
+    const key = req.params.key;
+    res.render("register",{ename : event, keys : key});
+});
+
+app.get("/contact",(req,res)=>{
+    res.render("contact");
+});
+app.get("/thankyou",(req,res)=>{
+    res.render("thankyou");
+});
+app.post("/register",function(req,res){
+    const event = req.body.event;
+    const key = req.body.decider;
+    res.redirect(`/register/${event}/${key}`);
+});
+
+app.post("/",(req,res)=>{
+    if(req.body.key===1){
+        const user =  new rUser({
+            eventName : req.body.event,
+            t1Name : req.body.t1name,
+            t1Rno : req.body.t1no,
+            email : req.body.email,
+            mobile : req.body.mobile
+        });
+        user.save();
+    }
+    else if(req.body.key===2){
+        const user =  new rUser({
+            eventName : req.body.event,
+            t1Name : req.body.t1name,
+            t1Rno : req.body.t1no,
+            t2Name : req.body.t2name,
+            t2Rno : req.body.t2no,
+            email : req.body.email,
+            mobile : req.body.mobile
+        });
+        user.save();
+    }
+    else{
+        const user =  new rUser({
+            eventName : req.body.event,
+            t1Name : req.body.t1name,
+            t1Rno : req.body.t1no,
+            t2Name : req.body.t2name,
+            t2Rno : req.body.t2no,
+            t3Name : req.body.t3name,
+            t3Rno : req.body.t3no,
+            t4Name : req.body.t4name,
+            t4Rno : req.body.t4no,
+            email : req.body.email,
+            mobile : req.body.mobile
+        });
+        user.save();
+    }
+    res.render("thankyou");
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
+  
---
+++ register.js
@@ -0,0 +1,60 @@
+
+// function helper(){
+//     const decider = $("#reg").text();
+//       console.log(decider);
+
+      function addMember2(){
+        $(".form-details").append(
+          <div class="t2">
+              <div class="t2name" id="div">
+                <label for="T2" id="form-label">Team Member 2: </label>
+                <input type="text" name="t2name"  placeholder=" " />
+              </div>
+              <div class="t2rno" id="div">
+                <label for="rno" id="form-label">Team Member 2 roll number:</label>
+                <input type="text" name="t2no" />
+              </div>
+            </div>
+        );
+      }
+
+      function addMember3(){
+        $('.form-details').append(
+          <div class="t3">
+              <div class="t3name" id="div">
+                <label for="T3" id="form-label">Team Member 3: </label>
+                <input type="text" name="t2name"  placeholder=" " />
+              </div>
+              <div class="t3rno" id="div">
+                <label for="rno" id="form-label">Team Member 3 roll number:</label>
+                <input type="text" name="t3no" />
+              </div>
+            </div>
+        );
+      }
+
+      function addMember4(){
+        $('.form-details').append(
+          <div class="t4">
+              <div class="t4name" id="div">
+                <label for="T4" id="form-label">Team Member 4: </label>
+                <input type="text" name="t4name"  placeholder=" " />
+              </div>
+              <div class="t4rno" id="div">
+                <label for="rno" id="form-label">Team Member 4 roll number:</label>
+                <input type="text" name="t4no" />
+              </div>
+            </div>
+        );
+      }
+      if(decider==="2"){
+        addMember2();
+      }
+      else if(decider==="4"){
+        addMember2();
+        addMember3();
+        addMember4();
+      }
+// }
+
+// helper();

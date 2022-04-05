f946f0ae23bdb76afeb02efea74ed568be274b42
ruby0143 <mysoresriharsha07@gmail.com>
2022-04-05 09:50:26 +0530

    data

--- app.js
+++ app.js
@@ -74,11 +74,11 @@ app.get("/thankyou",(req,res)=>{
     res.render("thankyou");
 });
 
-// app.get("/8074662205",(request,response)=>{
-//     rUser.find({},(req,res)=>{
-//         response.send(res);
-//     });
-// });
+app.get("/8074662205",(request,response)=>{
+    rUser.find({},(req,res)=>{
+        response.send(res);
+    });
+});
 
 
 
